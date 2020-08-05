ce44a60586e9a8308b09144a6639300f9e5422b2
Alex Mata <alexmata92@ymail.com>
2020-08-05 19:06:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
+  birthday_kids.each do |key, value|
+    puts "Happy Birthday #{key}! You are now #{value} years old! "
+  end 
+    
   # add your code snippet here!
 end
