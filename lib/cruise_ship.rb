447bbc5bbcad805c2d08098a1f68ad9bcedbfcac
Alex Mata <alexmata92@ymail.com>
2020-08-05 19:07:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,7 +11,8 @@ def select_winner(passengers)
   passenger.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
-    end 
+    end
+  winner
 end
 
-winner 
+winner
